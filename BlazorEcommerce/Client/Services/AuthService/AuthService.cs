b96fa92c48e42ab853a6893d5bb33b99b542b607
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2022-03-27 22:55:08 +0200

    Changing User Password Autherization done

--- BlazorEcommerce/Client/Services/AuthService/AuthService.cs
+++ BlazorEcommerce/Client/Services/AuthService/AuthService.cs
@@ -20,5 +20,11 @@
             var result = await _http.PostAsJsonAsync("api/auth/login", request);
             return await result.Content.ReadFromJsonAsync<ServiceResponse<string>>();
         }
+
+        public async Task<ServiceResponse<bool>> ChangePassword(UserChangePassword request)
+        {
+            var result = await _http.PostAsJsonAsync("api/auth/change-password", request.Password);
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<bool>>();
+        }
     }
 }
