1877d3662e5489bed1018aa04173d647f909351d
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2022-04-02 15:49:59 +0200

    ProductTypes for Admin role on the Server side

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -10,6 +10,7 @@ global using BlazorEcommerce.Server.Services.OrderService;
 global using BlazorEcommerce.Shared.DTO;
 global using BlazorEcommerce.Server.Services.PaymentService;
 global using BlazorEcommerce.Server.Services.AddressService;
+global using BlazorEcommerce.Server.Services.ProductTypeService;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -35,6 +36,7 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IProductTypeService, ProductTypeService>();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
