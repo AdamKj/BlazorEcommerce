00d607f45fa077108901d3dc44f5d832b7d0e9cb
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2022-04-02 16:22:34 +0200

    POST and PUT operations on the Server side

--- BlazorEcommerce/Server/Services/ProductTypeService/ProductTypeService.cs
+++ BlazorEcommerce/Server/Services/ProductTypeService/ProductTypeService.cs
@@ -14,5 +14,30 @@
             var productTypes = await _context.ProductTypes.ToListAsync();
             return new ServiceResponse<List<ProductType>> {Data = productTypes};
         }
+
+        public async Task<ServiceResponse<List<ProductType>>> AddProductType(ProductType productType)
+        {
+            _context.ProductTypes.Add(productType);
+            await _context.SaveChangesAsync();
+            return await GetProductTypes();
+        }
+
+        public async Task<ServiceResponse<List<ProductType>>> UpdateProductType(ProductType productType)
+        {
+            var dbProductType = await _context.ProductTypes.FindAsync(productType.Id);
+            if (dbProductType is null)
+            {
+                return new ServiceResponse<List<ProductType>>
+                {
+                    Success = false,
+                    Message = "Product Type not found."
+                };
+            }
+
+            dbProductType.Name = productType.Name;
+            await _context.SaveChangesAsync();
+
+            return await GetProductTypes();
+        }
     }
 }
