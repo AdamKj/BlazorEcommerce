ebad0d7dfdcfc3b3e52962b69d59cc074e5908c3
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2022-04-02 11:06:29 +0200

    Address Service and Controller implemented on the Server

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -9,6 +9,7 @@ global using BlazorEcommerce.Server.Services.AuthService;
 global using BlazorEcommerce.Server.Services.OrderService;
 global using BlazorEcommerce.Shared.DTO;
 global using BlazorEcommerce.Server.Services.PaymentService;
+global using BlazorEcommerce.Server.Services.AddressService;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -33,6 +34,7 @@ builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddScoped<IAddressService, AddressService>();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
---
+++ BlazorEcommerce/Server/Services/AddressService/AddressService.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.Loader;
+
+namespace BlazorEcommerce.Server.Services.AddressService
+{
+    public class AddressService : IAddressService
+    {
+        private readonly DataContext _context;
+        private readonly IAuthService _authService;
+
+        public AddressService(DataContext context, IAuthService authService)
+        {
+            _context = context;
+            _authService = authService;
+        }
+        public async Task<ServiceResponse<Address>> GetAddress()
+        {
+            int userId = _authService.GetUserId();
+            var address = await _context.Addresses.FirstOrDefaultAsync(a => a.UserId == userId);
+            return new ServiceResponse<Address> {Data = address};
+        }
+
+        public async Task<ServiceResponse<Address>> AddOrUpdateAddress(Address address)
+        {
+            var response = new ServiceResponse<Address>();
+            var dbAddress = (await GetAddress()).Data;
+            if (dbAddress is null)
+            {
+                address.UserId = _authService.GetUserId();
+                _context.Addresses.Add(address);
+                response.Data = address;
+            }
+            else
+            {
+                dbAddress.FirstName = address.FirstName;
+                dbAddress.LastName = address.LastName;
+                dbAddress.Street = address.Street;
+                dbAddress.City = address.City;
+                dbAddress.Country = address.Country;
+                dbAddress.Zip = address.Zip;
+            }
+
+            await _context.SaveChangesAsync();
+
+            return response;
+        }
+    }
+}
